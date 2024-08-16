faf601cdcd3261894090a5c8ab4c4cdf0e4fd1a6
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-08-16 16:49:16 -0300

    Task 07 e 08

---
+++ Task07/script.js
@@ -0,0 +1,6 @@
+function trocar(){
+    const html = document.documentElement
+    const body = document.getElementById("bb")
+    html.classList.toggle("light")
+    body.classList.toggle("lightBody")
+}
